cbc413accf3c59dfdef3c4158ff16099fc79f5a1
clpiano <111512024+clpiano@users.noreply.github.com>
2022-11-27 15:09:14 +0900

    env

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 Restaurant.create!(
   name: 'まくらパスタ',
   email: 'makura@mail',
-  password: ENV['makura'],
+  password: ENV['PASSWORD'],
   genre: '洋食',
   zipcode: '1234567',
   address: '神奈川県○○市○○町１－２－３',
